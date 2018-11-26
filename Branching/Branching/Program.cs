5120b71833385fd6f29d03bfa03bdd19605cb0c3
Ron Silva <ron_silva@outlook.com>
2018-11-25 20:15:43 -0800

    Add Branching sample

---
+++ Branching/Branching/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentTempature = 85;
+            int roomTempature = 80;
+
+            //string comparisonResult = currentTempature == roomTempature ? "It is room temp." : "It is not room temp.";
+            //Console.WriteLine(comparisonResult);
+            
+            //if (currentTempature == roomTempature)
+            //{
+            //    Console.WriteLine("It is exactly room tempature.");
+            //}
+            //else if (currentTempature > roomTempature)
+            //{
+            //    Console.WriteLine("It is warmer than room tempature.");
+            //}
+            //else if (roomTempature > currentTempature)
+            //{
+            //    Console.WriteLine("Room tempature is wamer than current tempature.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("It is not exactly room tempature.");
+            //}
+            //Console.Read();
+        }
+    }
+}
