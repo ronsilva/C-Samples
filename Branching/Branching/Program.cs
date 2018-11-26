07aaed09321512816cdb5cd4d9892435dc340eee
Ron Silva <ron_silva@outlook.com>
2018-11-25 20:39:11 -0800

    Update Program.cs

--- Branching/Branching/Program.cs
+++ Branching/Branching/Program.cs
@@ -10,12 +10,46 @@ namespace Branching
     {
         static void Main(string[] args)
         {
-            int currentTempature = 85;
-            int roomTempature = 80;
+            //Console.WriteLine("What is your favorite number?");
+            //int favNum = Convert.ToInt32(Console.ReadLine());
+
+            //string result = favNum == 12 ? "you have an awesome favorite number." : "You do not have an awesome favorite number.";
+            //Console.WriteLine(result);
+            //Console.ReadLine();
+            
+
+            //int roomTempature = 70;
+
+            //Console.WriteLine("Hi, what is your name?");
+            //string name = Console.ReadLine();
+
+            //Console.WriteLine("Hi, " + name + ", what is the tempature where you are?");
+            //int currentTempature = Convert.ToInt32(Console.ReadLine());
+
+            //if (currentTempature == roomTempature)
+            //{
+            //    Console.WriteLine("It is exactly room tempature.");
+            //}
+            //else if (currentTempature > roomTempature)
+            //{
+            //    Console.WriteLine("It is warmer than room tempature.");
+            //}
+            //else if (roomTempature < currentTempature)
+            //{
+            //    Console.WriteLine("It is colder than room tempature.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Uhhhh.... Something went wrong.");
+            //}
+            //Console.ReadLine();
+
+            //int currentTempature = 85;
+            //int roomTempature = 80;
 
             //string comparisonResult = currentTempature == roomTempature ? "It is room temp." : "It is not room temp.";
             //Console.WriteLine(comparisonResult);
-            
+
             //if (currentTempature == roomTempature)
             //{
             //    Console.WriteLine("It is exactly room tempature.");
@@ -33,6 +67,7 @@ namespace Branching
             //    Console.WriteLine("It is not exactly room tempature.");
             //}
             //Console.Read();
+
         }
     }
 }
