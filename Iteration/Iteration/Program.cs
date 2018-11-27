2e68ae85f41de427946d6c3c561decee2461307c
Ron Silva <ron_silva@outlook.com>
2018-11-26 22:54:13 -0800

    Add Iteration Sample

---
+++ Iteration/Iteration/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        //int[] testScores = {86, 87, 99, 52, 85, 75, 98, 65, 97};
+
+        //for (int i = 0; i < testScores.Length; i++)
+        //{
+        //    if (testScores[i] > 85)
+        //    {
+        //        Console.WriteLine("Passing test score: " + testScores[i]);
+        //    }
+
+        //}
+
+        //string[] names = { "Jesse", "Ted", "Bill", "Katy", "Ron" };
+
+        //for (int j = 0; j < names.Length; j++)
+        //{
+        //    if (names[j] == "Ron")
+        //    {
+        //        Console.WriteLine(names[j]);
+        //    }
+
+        //}
+        //Console.ReadLine();
+
+        //string[] names = { "Jesse", "Ted", "Bill", "Katy", "Ron" };
+
+        //for (int j = 0; j < names.Length; j++)
+        //{
+        //    Console.WriteLine(names[j]);
+        //}
+
+        //List<int> testScores = new List<int>();
+        //testScores.Add(98);
+        //testScores.Add(99);
+        //testScores.Add(81);
+        //testScores.Add(72);
+        //testScores.Add(78);
+
+        //foreach (int score in testScores)
+        //{
+        //    if (score > 85)
+        //    {
+        //        Console.WriteLine("Passing test score: " + score);
+        //    }       
+        //}
+
+        //List<string> names = new List<string>() { "Ron", "Bill", "Ted", "Donna", "Jesse" };
+
+        //foreach (string name in names)
+        //{
+        //    Console.WriteLine(name);
+        //}
+
+        //List<int> testScores = new List<int>() { 98, 99, 12, 74, 23, 99, };
+        //List<int> passingScores = new List<int>();
+
+        //foreach (int score in testScores)
+        //{
+        //    if (score > 85)
+        //    {
+        //        passingScores.Add(score);
+        //    }
+        //}
+
+        //Console.WriteLine(passingScores.Count);
+
+        //Console.ReadLine();
+
+        
+    }
+}
+
