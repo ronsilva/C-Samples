359abe5260bc76f3874fbff2b4274838b83b6115
Ron Silva <ron_silva@outlook.com>
2018-12-04 21:53:58 -0800

    Add exception handling sample

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Pick a numer:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick another numer:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dividing by two... ");
+            Console.WriteLine(num1 + " devided by " + num2 + " equals " + num1 / num2);
+            Console.ReadLine();
+        }
+        catch (FormatException ex)
+        {
+            //Console.WriteLine(ex.Message);
+            Console.WriteLine("Please enter a whole number");
+            return;
+        }
+
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Please don't devide by zero");
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.ReadLine();
+        }
+
+    }
+}
